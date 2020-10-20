e98ece5a0f9c915189f2df9c704eb0f32a244077
BillRigg <riggiobill@gmail.com>
2020-10-20 15:53:24 -0400

    Copy-pasted code in from IPYNB
    Copied all relevant code from mission-to-mars.ipynb and made sure it fit the function format in scrape_mars.py. Needs testing once the app.py is set up.

--- Missions_to_Mars/scrape_mars.py
+++ Missions_to_Mars/scrape_mars.py
@@ -30,6 +30,7 @@ def scrape():
 
     #####
     ##  Task 1 : NASA Mars News
+    ##  Using code from mission_to_mars.ipynb, populate the dictionary
     #####
     url = "https://mars.nasa.gov/news/"
     browser.visit(url)
@@ -43,15 +44,75 @@ def scrape():
 
     mars_dict["mars_news_title"] = news_title
     mars_dict["mars_news_paragraph"] = news_paragraph
+    #--------------------------------------------------------------------------------------------
 
     #####
     ##  Task 2 : JPL Mars Space Imgs
+    ##  Using code from mission_to_mars.ipynb, populate the dictionary
     #####
+    jpl_url = "https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars"
+    browser.visit(jpl_url)
+
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Find first featured Mars image
+    image = soup.find("img",class_="thumb")["src"]
+
+    # Find image url to full size .jpg image and append to initial url
+    image_url = "https://jpl.nasa.gov"+image
+    featured_image_url = image_url
+
+    mars_dict["featured_image_url"] = featured_image_url
+    #--------------------------------------------------------------------------------------------
 
     #####
     ##  Task 3 : Mars Facts
+    ##  Using code from mission_to_mars.ipynb, populate the dictionary
     #####
+
+    facts_url = "https://space-facts.com/mars/"
+    browser.visit(facts_url)
+
+    facts_pd = pd.read_html(facts_url)
+    facts_df = pd.DataFrame(facts_pd[0])
+
+    facts_df.columns=['Attribute','Data']
+
+    # Use Pandas to convert the data into an HTML table string using Pandas .to_html and .replace
+    facts_table = facts_df.set_index("Attribute")
+    mars_facts = facts_table.to_html(classes = 'mars_facts')
+    mars_facts = mars_facts.replace('\n', ' ')
+
+    mars_dict["html_table_string"] mars_facts
+    #--------------------------------------------------------------------------------------------
 
     #####
     ##  Task 4 : Mars Hemispheres
+    ##  Using code from mission_to_mars.ipynb, populate the dictionary
     #####
+
+    hemi_url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(hemi_url)
+
+    html = browser.html
+    soup = BeautifulSoup(html, 'html.parser')
+    hemispheres = []
+
+    for x in range (4):
+        image_array = browser.find_by_tag('h3')
+        image_array[x].click()
+        html = browser.html
+        soup = BeautifulSoup(html, 'html.parser')
+        src = soup.find("img", class_="wide-image")["src"]
+        x_url = 'https://astrogeology.usgs.gov'+src
+        x_title = soup.find("h2", class_="title").text
+        hemi_dict = {"title":x_title,"img url":x_url}
+        hemispheres.append(hemi_dict)
+        browser.back()
+
+    mars_dict["Mars_img_dict"] = hemispheres
+    #--------------------------------------------------------------------------------------------
+
+    # Return mars_dict with all scraped information
+    return mars_dict
