9a3f09f69a187060f7865f66facafedb028babe5
BillRigg <riggiobill@gmail.com>
2020-10-20 15:58:43 -0400

    Framework for app.py
    Skeleton set up for app.py. Needs route paths defined while mongo is running for testing.

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,1 +1,29 @@
-print("test")
+from flask import Flask, jsonify, render_template
+from flask_pymongo import PyMongo
+
+
+#####
+## Pull in the scrape_mars.py page to access the scrape function
+#####
+import scrape_mars
+
+app = Flask(__name__)
+mongo = PyMongo(app)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
